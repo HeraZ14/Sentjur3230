2fb3806354fa9a9a8c12c044bcaebec9f094afb3
HeraZ14 <vid.vaukan@xlab.si>
2025-05-27 16:03:56 +0200

    Postavitev okolja

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from store.models import Order, Category, Customer, Product
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+#Kategorije produktov
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+#Stranke
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+
+#Produkti
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,default=1)
+    description = models.TextField(default="", blank=True,null=True)
+    image = models.ImageField(upload_to='uploads/product/')
+    def __str__(self):
+        return self.name
+
+#Naročila strank
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    address = models.TextField(max_length=100, default="", blank=True,null=True)
+    phone = models.CharField(max_length=100,default="",blank=True,null=True)
+    date = models.DateTimeField(default=datetime.datetime.now)
+    status = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.customer} - {self.product}"
