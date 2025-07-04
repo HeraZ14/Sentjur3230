5cc73007d9d875b161b49e34a5269a5564c32103
Herazan <ziga.heric@gmail.com>
2025-07-04 17:26:35 +0200

    Končana osnovna vizualizacija za registracijsko formo/pozabljeno geslo itd... vse kar paše zraven

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import profile_view, custom_login, custom_logout
+from .views import profile_view, custom_login, custom_logout, CustomPasswordResetDoneView, CustomPasswordResetCompleteView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -12,12 +12,14 @@ urlpatterns = [
     # Forgot password flow:
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),
          name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+    path('password-reset/done/', CustomPasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
          name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-         name='password_reset_complete'),
+    path('reset/done/', CustomPasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LogoutView, LoginView
+from django.contrib.auth.views import LogoutView, LoginView, PasswordResetDoneView, PasswordResetCompleteView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from .forms import CustomUserCreationForm
@@ -13,6 +13,8 @@ def register(request):
             user = form.save()
             login(request, user)  # avtomatska prijava
             return redirect('vojzek')  # ali kamorkoli
+        else:
+            messages.warning(request, "Podatki so manjkajoči ali pa jih ni.")
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
@@ -28,5 +30,19 @@ class custom_login(LoginView):
             messages.success(request, "Odjava uspešna. Hvala za obisk metropole.")
         return super().get(request, *args, **kwargs)
 
+    def form_invalid(self, form):
+        messages.error(self.request, "Nepravilno uporabniško ime ali geslo.")
+        return super().form_invalid(form)
+
 class custom_logout(LogoutView):
     next_page = '/accounts/login/?logout=1'
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    def get(self, request, *args, **kwargs):
+        messages.success(request, "Če si pravilno vpisal e-mail naslov, si dobil navodila za ponastavitev gesla v tvoj poštni nabiralnik.")
+        return super().get(request, *args, **kwargs)
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    def get(self, request, *args, **kwargs):
+        messages.success(request, "Vaše geslo je bilo uspešno ponastavljeno. Sedaj se lahko prijavite")
+        return super().get(request, *args, **kwargs)
